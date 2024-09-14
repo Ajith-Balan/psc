1664e971f8f6efbd75de051b4977e8a5a38f45fb
AJITH B <139620991+Ajith-Balan@users.noreply.github.com>
2024-09-14 23:33:05 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import {useState} from 'react'
 
 
-import AddQuestion from './components/AddQuestion';
-import showquestion from './components/showquestion';
-import UpdateQuestion from './components/UpdateQuestion';
+
 import home from './components/home';
 
 
@@ -19,11 +17,6 @@ return(
 <BrowserRouter>
 <Routes>
                
-                    <Route path="/addquestion" Component={AddQuestion} />
-                    
-                    <Route path="/showquestion" Component={showquestion} />     
-
-                    <Route path="/updatequestion/:id" Component={UpdateQuestion} />
 
                     <Route path="/" Component={home} />
 
